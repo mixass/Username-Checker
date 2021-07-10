0ff506c39261d8090cf896cd4c47641e9120c8c5
mixass <67761405+mixass@users.noreply.github.com>
2021-07-10 12:04:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <html>
     <style>
-    h1 {
+    .title {
         font-weight: bold;
+        font-family:monospace;
         margin: 0;
     }
     input {
@@ -32,14 +33,29 @@
         color: #001234;
 
     }
-
+    .available {
+        background-color:lime;
+        height: 20px;
+        -moz-border-radius:75px;
+        -webkit-border-radius: 75px;
+        width: 20px;
+        display:inline-flex;
+    }
+    .used {
+        background-color:red;
+        height: 20px;
+        -moz-border-radius:75px;
+        -webkit-border-radius: 75px;
+        width: 20px;
+        display:inline-flex;
+    }
     </style>
     <body>
-        <center>
-            <h1>Username Checker</h1><br>
+        <center><br><br>
+            <h1 class='title'>Username Checker</h1><br>
             <form action="#" method="post">
                 <input type="text" name="searchName" autocomplete="off" row=200 placeholder="Find an available username. Search here. Example MIXAS"><br><br>
-                <button class="btn">Search</button>
+                <button class="btn">Search</button><br><br>
             </form>
         </center>
     </body>
@@ -47,49 +63,58 @@
 <?php
     $username = $_POST['searchName'];
     if (isset($username)) {
+        echo "<center>";
         CheckGithub($username);
         CheckYoutube($username);
         CheckBlogspot($username);
         CheckLinkTree($username);
+        CheckSteam($username);
+        echo "</center><br><br><br>";
     }
     function CheckGithub($username){
         $github = "https://www.github.com/$username";
         $response = file_get_contents($github);
         if($response == ''){
-            echo "Github kullanılmıyor";
+            echo "<div class='available'></div>Github<br><br>";
         }else {
-            echo "Bu kullanıcı adına sahip bir github bulunmakta";
+            echo "<div class='used'></div>Github<br><br>";
         }
-        echo "<br>";
     }
     function CheckYoutube($username){
         $youtube = "https://www.youtube.com/user/$username";
         $response = file_get_contents($youtube);
         if($response == ''){
-            echo "Youtube kullanılmıyor";
+            echo "<div class='available'></div>Youtube<br><br>";
         }else {
-            echo "Bu kullanıcı adına sahip bir youtube bulunmakta";
+            echo "<div class='used'></div>Youtube<br><br>";
         }
-        echo "<br>";
     }
     function CheckBlogspot($username){
         $blogspot = "http://$username.blogspot.com";
         $response = file_get_contents($blogspot);
         if($response == ''){
-            echo "blogspot kullanılmıyor";
+            echo "<div class='available'></div>Blogspot<br><br>";
         }else {
-            echo "Bu kullanıcı adına sahip bir blogspot bulunmakta";
+            echo "<div class='used'></div>Blogspot<br><br>";
         }
-        echo "<br>";
     }
     function CheckLinkTree($username){
-        $twitter = "https://linktr.ee/$username";
-        $response = file_get_contents($twitter);
+        $linktree = "https://linktr.ee/$username";
+        $response = file_get_contents($linktree);
         if($response == ''){
-            echo "linktree kullanılmıyor";
+            echo "<div class='available'></div>Linktree<br><br>";
         }else {
-            echo "Bu kullanıcı adına sahip bir linktree bulunmakta";
+            echo "<div class='used'></div>Linktree<br><br>";
         }
-        echo "<br>";
     }
+    function CheckSteam($username){
+        $linktree = "https://steamcommunity.com/id/$username";
+        $response = file_get_contents($linktree);
+        if(strpos($response,'Sorry!')){
+            echo "<div class='available'></div>Steam<br><br>";
+        }else {
+            echo "<div class='used'></div>Steam<br><br>";
+        }
+    }
+    
 ?>
